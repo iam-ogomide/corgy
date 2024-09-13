742036be3be8cdbfeb2e0bba8629497a6861631c
dogomide <dogomide@aiicoplc.com>
2024-09-14 00:07:33 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { CircleComponent } from './circle/circle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CircleComponent
   ],
   imports: [
     BrowserModule,
