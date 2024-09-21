292543148d3f13988e692f3203a4d15fb8ac3c5d
dogomide <dogomide@aiicoplc.com>
2024-09-21 03:04:40 +0100

    blog complete

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
+  activeIndex: number | null = null;
+
+  accordionData = [
+    { title: 'Introduction', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur architecto dolore impedit quam, quos maxime veniam. Quisquam exercitationem numquam nesciunt quod unde amet reiciendis repellat commodi esse. Ducimus, accusamus.' },
+    { title: 'Choosing The Best Fits', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur architecto dolore impedit quam, quos maxime veniam. Quisquam exercitationem numquam nesciunt quod unde amet reiciendis repellat commodi esse. Ducimus, accusamus.' },
+    { title: 'Fashion Tips', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur architecto dolore impedit quam, quos maxime veniam. Quisquam exercitationem numquam nesciunt quod unde amet reiciendis repellat commodi esse. Ducimus, accusamus.' },
+    { title: 'More Styling Resources', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid aspernatur architecto dolore impedit quam, quos maxime veniam. Quisquam exercitationem numquam nesciunt quod unde amet reiciendis repellat commodi esse. Ducimus, accusamus.' },
+  ];
+
+  toggleSection(index: number): void {
+    this.activeIndex = this.activeIndex === index ? null : index;
+  }
 
   constructor() { }
 
